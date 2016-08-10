f10aad70987172f9b669a86a46f86259e13b49de
“魏金龙” <jlwei@qq.com>
2016-08-09 22:13:15 -0700

    redis db change

--- game/lualib/base/util.lua
+++ game/lualib/base/util.lua
@@ -60,3 +60,40 @@ function assin_server_id()
 	server_id = server_id + 1
 	return server_id
 end
+
+
+-------serialize table----------------------------
+function serialize_table(t)
+	local mark={}
+	local assign={}
+	local function ser_table(tbl,parent)
+		mark[tbl]=parent
+		local tmp={}
+		for k,v in pairs(tbl) do
+			local key = (type(k)=="number" and "["..k.."]") or k
+			if type(v)=="table" then
+				local dotkey = parent..type(k)=="number" and key or ("."..key)
+				if mark[v] then
+					table.insert(assign,dotkey.."="..mark[v])
+				else
+					table.insert(tmp, key.."="..ser_table(v,dotkey))
+				end
+			elseif type(v) == "string" then
+				table.insert(tmp, key.."='"..v.."'")
+			elseif type(v) == 'boolean' then
+				if v then
+					table.insert(tmp, key.."=true")
+				else
+					table.insert(tmp, key.."=false")
+				end
+			elseif type(v) == 'number' then
+				table.insert(tmp, key.."="..v)
+			end
+		end
+		return "{"..table.concat(tmp,",").."}"
+		
+	end
+	
+	return "do local ret="..ser_table(t,"ret")..table.concat(assign," ").." return ret end"
+end
+
--- game/lualib/db/cards.lua
+++ game/lualib/db/cards.lua
@@ -1,3 +1,4 @@
+local syslog = require "syslog"
 local cards = {}
 local connection_handler
 
@@ -9,37 +10,31 @@ local function make_key (name)
 	return connection_handler (name), string.format ("cards:%s", name)
 end
 
-function cards.load (name)
-	assert (name)
-	local connection, key = make_key (name)
-	local tb = nil
+function cards.load (account)
+	local connection, key = make_key (account)
+	local tb = {}
 	if connection:exists (key) then
-		local length = connection:llen(key)
-		tb = connection:lrange(key,0,length)	
+		local st = connection:smembers(key)
+		for k, v in pairs(st) do
+			table.insert(tb, load(v)())
+		end
 	end
 	return tb
 end
 
 --添加卡牌
-function cards.addCard(name,id,num)
-	assert(name and id)
-	num  = num or 1
-	local connection,key = make_key(name)
-	if connection:exists (key)  then
-		conneciton:zincrby(key,num,id)
-	else
-		conneciton:zadd(key,num,id)
-	end
+function cards.addCard(account, tb)
+	assert(account and tb)
+	local connection,key = make_key(account)
+	connection:sadd(key, serialize_table(tb))
 end
 
 --删除卡牌
-function cards.delCard(name,id,num)
-	assert(name and id)
-	num  =  num or 1
-	num  = - num
-	if connection:exists (key)  then
-		conneciton:zincrby(key,num,id)
-	end
+function cards.delCard(account, tb)
+	assert(account and tb)
+	local connection,key = make_key(account)
+	print(serialize_table(tb))
+	connection:srem(key, serialize_table(tb))
 end
 
 return cards
