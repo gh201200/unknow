9a2b5aed88b86653ca61e5dd526a87159343388a
fangjun <fang@example.com>
2016-10-20 14:43:06 -0400

    skilldata修改

--- game/lualib/skill/Affects/blinkAffect.lua
+++ game/lualib/skill/Affects/blinkAffect.lua
@@ -6,14 +6,15 @@ function blinkAffect:ctor(entity,source,data)
 	self.distance = self.data[2] or 0
 	self.effectTime = self.data[3] or 0
 	self.effectId = self.data[4] or 0
+	self.target = self.source:getTarget()
 end
 
 function blinkAffect:onEnter()
 	--强制设置目标位置
-	self.super.onEnter(self)	
+	self.super.onEnter(self)
 	local distance  = self.distance --闪现距离
 	local vec = self.owner.dir:return_mul_num(distance)
-	local  des = self.owner:getTarget().pos--self.owner.pos:return_add(vec)
+	local  des = self.target.pos--self.owner.pos:return_add(vec)
 	self.owner:onBlink(des)
 end
 
