6f5c4eb46b813c35eb8558d85f714db448041e6d
fangjun <fang@example.com>
2016-11-12 08:59:36 -0500

    技能升级效果无效bug修改

--- game/service/room.lua
+++ game/service/room.lua
@@ -71,7 +71,7 @@ end
 
 function CMD.requestCastSkill(response,agent, account_id, args)
 	local player = EntityManager:getPlayerByPlayerId(account_id)
-	local skillId = args.skillid-- + player.skillTable[args.skillid] - 1
+	local skillId = args.skillid + player.skillTable[args.skillid] - 1
 	local err = player:setCastSkillId(skillId)
 	response(true, { errorcode =  err ,skillid = skillId })
 end
