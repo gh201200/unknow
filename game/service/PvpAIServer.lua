2dd548b48d276cf0a25c411bed5d63890769e4d1
root <root@192.168.0.150>
2017-02-25 17:29:49 +0800

    del swo swp file

---
+++ game/service/PvpAIServer.lua
@@ -0,0 +1,32 @@
+local skynet = require "skynet"
+local watchdog 
+local CMD = {}
+
+local match
+local players = {}
+function CMD.getPvpAIs(num)
+	local AIs = {}
+	local accouts = { "robot1" ,"robot2","robot3"}
+	--accouts = { "robot1" }
+	for k, v in pairs(accouts) do
+		local agent = skynet.call(watchdog,"lua","authAi",v)
+		skynet.call(agent,"lua","Request","enterGame")
+		--开始匹配
+		local p = skynet.call(agent,"lua","getmatchinfo")
+		p.isAI = true
+		table.insert(AIs,p)
+	end
+	--print("======AIs",#AIs)
+	return AIs		
+end
+
+function CMD.start(w)
+	watchdog = w
+end
+skynet.start(function()
+	skynet.dispatch("lua",function(_,_,command,...)
+		local f = CMD[command]
+		local r = f(...)
+		return skynet.ret(skynet.pack(r))
+	end)
+end)
