9d178180e0d9fbe9e52a3c26c2bd653fed97ffc8
root <root@192.168.0.150>
2017-04-11 14:43:18 +0800

    宠物死亡被动技能移除

--- game/lualib/entity/Ipet.lua
+++ game/lualib/entity/Ipet.lua
@@ -132,7 +132,11 @@ end
 function IPet:onDead()
 	IPet.super.onDead(self)
 	g_entityManager:sendToAllPlayers("killEntity", {sid=self.serverId})
-	
+	for i=#(self.spell.passtiveSpells),1,-1 do
+		local v = self.spell.passtiveSpells[i]
+		v:onDead()
+		table.remove(self.spell.passtiveSpells,i)
+	end
 	--response to agent
 	for k, v in pairs(self.coroutine_response) do
 		for p, q in pairs(v) do
