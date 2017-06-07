343ca5506e904614f77e2383af7b5466f0e532e1
233 <233@qq.com>
2017-06-07 09:34:49 +0800

    修改外网监听端口

--- game/lualib/config/gameserver.lua
+++ game/lualib/config/gameserver.lua
@@ -1,12 +1,12 @@
 local config = {
 	name = "gameserver",
-	port = 8888,
+	port = 7777,
 	maxclient = 10000,
 	agent_pool = 10,
 }
 
-config.debug_port = 9333
-config.web_port = 8001
+config.debug_port = 9332
+config.web_port = 8002
 config.log_level = 2 -- 1:debug 2:info 3:notice 4:warning 5:error
 
 return config
